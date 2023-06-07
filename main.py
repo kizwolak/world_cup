874876370facd722731a292bd395c9d516343303
kizwolak <ki.zwolak@gmail.com>
2023-06-07 18:08:27 -0300

    Fix goal count and sort by goals scored

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ def goal_extractor(string):
         home_goles = string[4]
         away_goles = string[6]
         away_penal = string[9]
-        return [int(home_penal), int(home_goles), int(away_goles), int(away_penal)]
+        return {'home_penal': int(home_penal), 'home_goles': int(home_goles),
+                'away_goles': int(away_goles), 'away_penal': int(away_penal)}
     else:
         home_goles = string[0]
         away_goles = string[2]
-        return [int(home_goles), int((away_goles))]
+        return {'home_goles': int(home_goles), 'away_goles': int((away_goles))}
 
 
 def add_group(archive, processed_countries, country_name):
@@ -31,28 +32,28 @@ def calculate_ranks(lines, processed_countries):
         second_country_name = new_row_values[4]
         goals = goal_extractor(new_row_values[7])
         if new_row_values[3] == first_country_name:
-            if goals[0] > goals[1]:
+            if goals['home_goles'] > goals['away_goles']:
                 final_teams.append(first_country_name)
                 playoff_teams.append(second_country_name)
-            elif goals[1] > goals[0]:
+            elif goals['away_goles'] > goals['home_goles']:
                 final_teams.append(second_country_name)
                 playoff_teams.append(first_country_name)
             else:
-                if home_penal > away_penal:
+                if goals['home_penal'] > goals['away_penal']:
                     final_teams.append(first_country_name)
                     playoff_teams.append(second_country_name)
                 else:
                     final_teams.append(second_country_name)
                     playoff_teams.append(first_country_name)
         if new_row_values[4] == first_country_name:
-            if goals[1] > goals[0]:
+            if goals['away_goles'] > goals['home_goles']:
                 final_teams.append(first_country_name)
                 playoff_teams.append(second_country_name)
-            elif goals[0] > goals[1]:
+            elif goals['home_goles'] > goals['away_goles']:
                 final_teams.append(second_country_name)
                 playoff_teams.append(first_country_name)
             else:
-                if away_penal > home_penal:
+                if goals['away_penal'] > goals['home_penal']:
                     final_teams.append(first_country_name)
                     playoff_teams.append(second_country_name)
                 else:
@@ -64,7 +65,7 @@ def calculate_ranks(lines, processed_countries):
             new_row_values = line.strip().split(',')
             goals = goal_extractor(new_row_values[7])
             if playoff_teams[0] == new_row_values[3]:
-                if goals[0] > goals[1]:
+                if goals['home_goles'] > goals['away_goles']:
                     processed_countries[playoff_teams[0]]['rank'] = 3
                     processed_countries[playoff_teams[1]]['rank'] = 4
                 elif goals[1] > goals[0]:
@@ -82,10 +83,10 @@ def calculate_ranks(lines, processed_countries):
                         processed_countries[playoff_teams[1]
                                             ]['rank'] = 3
             if playoff_teams[0] == new_row_values[4]:
-                if goals[1] > goals[0]:
+                if goals['away_goles'] > goals['home_goles']:
                     processed_countries[playoff_teams[0]]['rank'] = 3
                     processed_countries[playoff_teams[1]]['rank'] = 4
-                elif goals[0] > goals[1]:
+                elif goals['home_goles'] > goals['away_goles']:
                     processed_countries[playoff_teams[0]]['rank'] = 4
                     processed_countries[playoff_teams[1]]['rank'] = 3
                 else:
@@ -105,14 +106,14 @@ def calculate_ranks(lines, processed_countries):
             new_row_values = line.strip().split(',')
             goals = goal_extractor(new_row_values[7])
             if final_teams[0] == new_row_values[3]:
-                if goals[0] > goals[1]:
+                if goals['home_goles'] > goals['away_goles']:
                     processed_countries[final_teams[0]]['rank'] = 1
                     processed_countries[final_teams[1]]['rank'] = 2
-                elif goals[1] > goals[0]:
+                elif goals['away_goles'] > goals['home_goles']:
                     processed_countries[final_teams[0]]['rank'] = 2
                     processed_countries[final_teams[1]]['rank'] = 1
                 else:
-                    if home_penal > away_penal:
+                    if goals['home_penal'] > goals['away_penal']:
                         processed_countries[final_teams[0]
                                             ]['rank'] = 1
                         processed_countries[final_teams[1]
@@ -123,10 +124,10 @@ def calculate_ranks(lines, processed_countries):
                         processed_countries[final_teams[1]
                                             ]['rank'] = 1
             if final_teams[0] == new_row_values[4]:
-                if goals[1] > goals[0]:
+                if goals['away_goles'] > goals['home_goles']:
                     processed_countries[final_teams[0]]['rank'] = 1
                     processed_countries[final_teams[1]]['rank'] = 2
-                elif goals[0] > goals[1]:
+                elif goals['home_goles'] > goals['away_goles']:
                     processed_countries[final_teams[0]]['rank'] = 2
                     processed_countries[final_teams[1]]['rank'] = 1
                 else:
@@ -142,6 +143,10 @@ def calculate_ranks(lines, processed_countries):
                                             ]['rank'] = 1
 
 
+def sort_item(item):
+    return item[1]
+
+
 def create_goal_ranking(dictionary):
     country_list = []
     goal_list = []
@@ -149,6 +154,7 @@ def create_goal_ranking(dictionary):
         country_list.append(key)
         goal_list.append(dictionary[key]['goals'])
     combined_list = sorted(zip(country_list, goal_list))
+    combined_list.sort(key=sort_item, reverse=True)
     return combined_list
 
 
@@ -178,16 +184,16 @@ def create_dictionary(archive):
                     line_counter += 1
                     goals = goal_extractor(new_row_values[7])
                     if new_row_values[3] == country_name:
-                        stats_processed['goals'] += goals[0]
+                        stats_processed['goals'] += goals['home_goles']
                     if new_row_values[4] == country_name:
-                        stats_processed['goals'] += goals[1]
+                        stats_processed['goals'] += goals['away_goles']
 
                     if int(match_time[6]) == 2:
                         if int(match_time[9]) > 3 or int(match_time[8]) >= 1:
                             continue
                         else:
                             if new_row_values[3] == country_name:
-                                if goals[0] > goals[1]:
+                                if goals['home_goles'] > goals['away_goles']:
                                     if line_counter == 1:
                                         stats_processed['points'][line_counter] += 3
                                     if line_counter == 2:
@@ -196,7 +202,7 @@ def create_dictionary(archive):
                                     if line_counter == 3:
                                         stats_processed['points'][line_counter] += (
                                             stats_processed['points'][line_counter - 1] + 3)
-                                if goals[0] == goals[1]:
+                                if goals['home_goles'] == goals['away_goles']:
                                     if line_counter == 1:
                                         stats_processed['points'][line_counter] += 1
                                     if line_counter == 2:
@@ -205,7 +211,7 @@ def create_dictionary(archive):
                                     if line_counter == 3:
                                         stats_processed['points'][line_counter] += (
                                             stats_processed['points'][line_counter - 1] + 1)
-                                if goals[0] < goals[1]:
+                                if goals['home_goles'] < goals['away_goles']:
                                     if line_counter == 1:
                                         stats_processed['points'][line_counter] += 0
                                     if line_counter == 2:
@@ -215,7 +221,7 @@ def create_dictionary(archive):
                                         stats_processed['points'][line_counter] += (
                                             stats_processed['points'][line_counter - 1] + 0)
                             elif new_row_values[4] == country_name:
-                                if goals[0] < goals[1]:
+                                if goals['away_goles'] > goals['home_goles']:
                                     if line_counter == 1:
                                         stats_processed['points'][line_counter] += 3
                                     if line_counter == 2:
@@ -224,7 +230,7 @@ def create_dictionary(archive):
                                     if line_counter == 3:
                                         stats_processed['points'][line_counter] += (
                                             stats_processed['points'][line_counter - 1] + 3)
-                                if goals[0] == goals[1]:
+                                if goals['away_goles'] == goals['home_goles']:
                                     if line_counter == 1:
                                         stats_processed['points'][line_counter] += 1
                                     if line_counter == 2:
@@ -233,7 +239,7 @@ def create_dictionary(archive):
                                     if line_counter == 3:
                                         stats_processed['points'][line_counter] += (
                                             stats_processed['points'][line_counter - 1] + 1)
-                                if goals[0] > goals[1]:
+                                if goals['away_goles'] < goals['home_goles']:
                                     if line_counter == 1:
                                         stats_processed['points'][line_counter] += 0
                                     if line_counter == 2:
@@ -244,7 +250,7 @@ def create_dictionary(archive):
                                             stats_processed['points'][line_counter - 1] + 0)
                     else:
                         if new_row_values[3] == country_name:
-                            if goals[0] > goals[1]:
+                            if goals['home_goles'] > goals['away_goles']:
                                 if line_counter == 1:
                                     stats_processed['points'][line_counter] += 3
                                 if line_counter == 2:
@@ -253,7 +259,7 @@ def create_dictionary(archive):
                                 if line_counter == 3:
                                     stats_processed['points'][line_counter] += (
                                         stats_processed['points'][line_counter - 1] + 3)
-                            if goals[0] == goals[1]:
+                            if goals['home_goles'] == goals['away_goles']:
                                 if line_counter == 1:
                                     stats_processed['points'][line_counter] += 1
                                 if line_counter == 2:
@@ -262,7 +268,7 @@ def create_dictionary(archive):
                                 if line_counter == 3:
                                     stats_processed['points'][line_counter] += (
                                         stats_processed['points'][line_counter - 1] + 1)
-                            if goals[0] < goals[1]:
+                            if goals['home_goles'] < goals['away_goles']:
                                 if line_counter == 1:
                                     stats_processed['points'][line_counter] += 0
                                 if line_counter == 2:
@@ -272,7 +278,7 @@ def create_dictionary(archive):
                                     stats_processed['points'][line_counter] += (
                                         stats_processed['points'][line_counter - 1] + 0)
                         elif new_row_values[4] == country_name:
-                            if goals[0] < goals[1]:
+                            if goals['away_goles'] > goals['home_goles']:
                                 if line_counter == 1:
                                     stats_processed['points'][line_counter] += 3
                                 if line_counter == 2:
@@ -281,7 +287,7 @@ def create_dictionary(archive):
                                 if line_counter == 3:
                                     stats_processed['points'][line_counter] += (
                                         stats_processed['points'][line_counter - 1] + 3)
-                            if goals[0] == goals[1]:
+                            if goals['away_goles'] == goals['home_goles']:
                                 if line_counter == 1:
                                     stats_processed['points'][line_counter] += 1
                                 if line_counter == 2:
@@ -290,7 +296,7 @@ def create_dictionary(archive):
                                 if line_counter == 3:
                                     stats_processed['points'][line_counter] += (
                                         stats_processed['points'][line_counter - 1] + 1)
-                            if goals[0] > goals[1]:
+                            if goals['away_goles'] > goals['home_goles']:
                                 if line_counter == 1:
                                     stats_processed['points'][line_counter] += 0
                                 if line_counter == 2:
